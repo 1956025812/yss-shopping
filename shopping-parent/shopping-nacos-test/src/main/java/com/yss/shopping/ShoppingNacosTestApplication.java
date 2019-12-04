9a50f74cf84826821c611cf5a90848d7e36b50c9
duxuebo@globalegrow.com <duxuebo@globalegrow.com>
2019-12-04 15:00:56 +0800

    添加服务注册

--- shopping-parent/shopping-nacos-test/src/main/java/com/yss/shopping/ShoppingNacosTestApplication.java
+++ shopping-parent/shopping-nacos-test/src/main/java/com/yss/shopping/ShoppingNacosTestApplication.java
@@ -2,7 +2,9 @@ package com.yss.shopping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ShoppingNacosTestApplication {
 
