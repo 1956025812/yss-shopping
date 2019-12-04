7a2da1a72f283c1fa824b036cd582f62aaef2ea2
duxuebo@globalegrow.com <duxuebo@globalegrow.com>
2019-12-04 17:36:37 +0800

    添加用户服务

--- shopping-parent/yss-shopping-user/yss-shopping-user-web/src/main/java/com/yss/shopping/YssShoppingUserWebApplication.java
+++ shopping-parent/yss-shopping-user/yss-shopping-user-web/src/main/java/com/yss/shopping/YssShoppingUserWebApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class YssShoppingUserApplication {
+public class YssShoppingUserWebApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(YssShoppingUserApplication.class, args);
+        SpringApplication.run(YssShoppingUserWebApplication.class, args);
     }
 
 }
