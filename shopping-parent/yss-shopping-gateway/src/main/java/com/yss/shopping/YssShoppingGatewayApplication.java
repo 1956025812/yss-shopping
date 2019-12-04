906d9368e3ee33f5bdfa8f3b83fe8b0a1fdd1dd6
duxuebo@globalegrow.com <duxuebo@globalegrow.com>
2019-12-04 16:31:08 +0800

    网关50%

--- shopping-parent/yss-shopping-gateway/src/main/java/com/yss/shopping/YssShoppingGatewayApplication.java
+++ shopping-parent/yss-shopping-gateway/src/main/java/com/yss/shopping/YssShoppingGatewayApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ShoppingNacosTestApplication {
+public class YssShoppingGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ShoppingNacosTestApplication.class, args);
+        SpringApplication.run(YssShoppingGatewayApplication.class, args);
     }
 
 }
