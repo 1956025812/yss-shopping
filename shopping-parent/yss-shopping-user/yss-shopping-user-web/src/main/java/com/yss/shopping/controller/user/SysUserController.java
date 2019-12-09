fc80075f7587e948dfe8256ada091334d209d6ea
duxuebo@globalegrow.com <duxuebo@globalegrow.com>
2019-12-09 18:50:07 +0800

    修改用户 批量修改用户状态

--- shopping-parent/yss-shopping-user/yss-shopping-user-web/src/main/java/com/yss/shopping/controller/user/SysUserController.java
+++ shopping-parent/yss-shopping-user/yss-shopping-user-web/src/main/java/com/yss/shopping/controller/user/SysUserController.java
@@ -37,7 +37,7 @@ public class SysUserController {
 
     @ApiOperation("根据用户ID查询用户信息")
     @GetMapping("/select/by/id")
-    public ResultVO<SysUserOutVo> selectSysUserById(@ApiParam(name = "用户ID", required = true) @RequestParam String uid) {
+    public ResultVO<SysUserOutVo> selectSysUserById(@ApiParam(value = "用户ID", required = true) @RequestParam String uid) {
         logger.info("根基用户ID查询用户信息, 请求参数为：[uid:{}]", uid);
         SysUser sysUser = this.sysUserService.selectUserById(uid);
         SysUserOutVo sysUserOutVo = new SysUserOutVo().toSysUserOutVo(sysUser);
@@ -47,7 +47,7 @@ public class SysUserController {
 
     @ApiOperation("新增用户对象")
     @PostMapping("/save")
-    public ResultVO<Void> saveSysUser(@ApiParam(name = "新增用户InVo对象", required = true) @RequestBody SysUserSaveInVo sysUserSaveInVo) {
+    public ResultVO<Void> saveSysUser(@ApiParam(value = "新增用户InVo对象", required = true) @RequestBody SysUserSaveInVo sysUserSaveInVo) {
         logger.info("新增用户对象，请求参数为：{}", FastJsonUtil.bean2Json(sysUserSaveInVo));
         SysUser sysUser = this.sysUserService.saveSysUser(sysUserSaveInVo.toSysUser(sysUserSaveInVo));
         return ResultVO.getSuccess("新增用户成功", new SysUserOutVo().toSysUserOutVo(sysUser));
@@ -56,7 +56,7 @@ public class SysUserController {
 
     @ApiOperation("修改用户对象 TODO")
     @PostMapping("/update")
-    public ResultVO<Void> updateSysUser(@ApiParam(name = "修改用户InVo对象", required = true) @RequestBody SysUserUpdateInVo sysUserUpdateInVo) {
+    public ResultVO<Void> updateSysUser(@ApiParam(value = "修改用户InVo对象", required = true) @RequestBody SysUserUpdateInVo sysUserUpdateInVo) {
         logger.info("修改用户对象，请求参数为：{}", FastJsonUtil.bean2Json(sysUserUpdateInVo));
         this.sysUserService.updateSysUser(sysUserUpdateInVo.toSysUser(sysUserUpdateInVo));
         return ResultVO.getSuccess("修改用户成功");
@@ -66,8 +66,8 @@ public class SysUserController {
     @ApiOperation("批量修改用户状态 TODO")
     @PostMapping("/update/status/batch")
     public ResultVO<SysUserOutVo> updateSysUserStatusBatch(
-            @ApiParam(name = "用户ID集合", required = true) @RequestParam Long[] uidList,
-            @ApiParam(name = "用户状态: 0-删除，1-启用，2-禁用", required = true) @RequestParam Integer userStatus) {
+            @ApiParam(value = "用户ID集合", required = true) @RequestParam Long[] uidList,
+            @ApiParam(value = "用户状态: 0-删除，1-启用，2-禁用", required = true) @RequestParam Integer userStatus) {
         logger.info("批量修改用户状态, 请求参数为：[uidList:{}, userStatus:{}]", FastJsonUtil.bean2Json(uidList), userStatus);
         this.sysUserService.updateSysUserStatusBatch(uidList, userStatus);
         return ResultVO.getSuccess("批量修改用户状态成功");
