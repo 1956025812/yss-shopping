44802e5347d9305c4ce2583d665c943be1065fb2
duxuebo@globalegrow.com <duxuebo@globalegrow.com>
2019-12-19 11:58:08 +0800

    网关

--- shopping-parent/yss-shopping-gateway/src/main/java/com/yss/shopping/YssShoppingGatewayApplication.java
+++ shopping-parent/yss-shopping-gateway/src/main/java/com/yss/shopping/YssShoppingGatewayApplication.java
@@ -1,25 +1,12 @@
 package com.yss.shopping;
 
-import com.yss.shopping.filter.RewritePathBuildGatewayFilterFactory;
-import com.yss.shopping.predicate.PathNoApiRoutePredicateFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Bean;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class YssShoppingGatewayApplication {
-
-    @Bean
-    public PathNoApiRoutePredicateFactory pathNoApiRoutePredicateFactory() {
-        return new PathNoApiRoutePredicateFactory();
-    }
-
-    @Bean
-    public RewritePathBuildGatewayFilterFactory rewritePathBuildGatewayFilterFactory() {
-        return new RewritePathBuildGatewayFilterFactory();
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(YssShoppingGatewayApplication.class, args);
