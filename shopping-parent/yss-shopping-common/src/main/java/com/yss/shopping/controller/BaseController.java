34d9d20ff63c0645358df3d077ace032c1502e4b
杜学博 <xuebo.du@zhenai.com>
2020-01-15 11:57:58 +0800

    用户服务添加统一异常处理

---
+++ shopping-parent/yss-shopping-common/src/main/java/com/yss/shopping/controller/BaseController.java
@@ -0,0 +1,25 @@
+package com.yss.shopping.controller;
+
+import com.yss.shopping.vo.ResultVO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * BaseController
+ */
+@ControllerAdvice
+@RestController
+public class BaseController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BaseController.class);
+
+    @ExceptionHandler(value = Exception.class)
+    public ResultVO globalExceptionHandle(Exception e) {
+        logger.error("发生异常, 异常信息如下:", e);
+        return ResultVO.getFailed("非常抱歉,服务器异常,请联系客服人员。");
+    }
+
+}
