823fbf32909d19fabf2ce68bc1b705fbcb5c9900
1956025812 <1956022812@qq.com>
2020-02-06 22:19:11 +0800

    整合调度工程

---
+++ shopping-parent/yss-shopping-schedule/src/main/java/com/yss/shopping/YssShoppingScheduleApplication.java
@@ -0,0 +1,13 @@
+package com.yss.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YssShoppingScheduleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YssShoppingScheduleApplication.class, args);
+    }
+
+}
